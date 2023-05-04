f46aab0ae63dcb032105391ca1ea722c04caafcc
Leo Deng <myst.dg@gmail.com>
2023-05-04 15:51:34 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
